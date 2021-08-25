10a0d92165b9536f70a13f7a2b734bc165cafaab
jaimebriesemeister <82653221+jaimebriesemeister@users.noreply.github.com>
2021-08-25 09:50:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // escreva o código para alterar a cor de fundo 
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+}
+
